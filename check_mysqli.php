84089547aa3ad1737daafb6945a4587d9303735f
root <root@ip-172-31-17-32.ec2.internal>
2024-02-07 05:17:04 +0000

    first commit

---
+++ check_mysqli.php
@@ -0,0 +1,9 @@
+<?php
+// Check if MySQLi extension is enabled
+if (extension_loaded('mysqli')) {
+    echo "MySQLi extension is installed and enabled.";
+} else {
+    echo "MySQLi extension is NOT enabled.";
+}
+?>
+
